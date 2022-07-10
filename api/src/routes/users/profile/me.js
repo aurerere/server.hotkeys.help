275fe68e950c9327d5051d442f293717997a8999
aurerere <aure.dumay@gmail.com>
2022-07-10 23:04:40 +0200

    fresh start

--- api/src/routes/users/profile/me.js
+++
@@ -1,7 +0,0 @@
-exports.me = function (req, res) {
-    return res.status(200).send({
-        status: 200,
-        message: `${req.user.username} successfully reached`,
-        user: req.user
-    });
-}
--- api/src/utils/auth/rsaKeys.js
+++
@@ -1,6 +0,0 @@
-const fs = require('fs');
-
-const privateKey = fs.readFileSync(__dirname + '../../../../jwt.key');
-
-module.exports = { privateKey };
-
